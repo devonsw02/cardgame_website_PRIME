f27e1e5a45cb7856b543ed83d0945ef755157ca5
devonbadour <devonsw02@gmail.com>
2025-12-17 14:12:21 -0600

    Finished!

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/cardgame_website_FINAL/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  plugins: [vue(),
+
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
