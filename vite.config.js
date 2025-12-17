a12bf63133f87a61029dc6530f25f77e644ab2e0
devonbadour <devonsw02@gmail.com>
2025-12-17 15:47:27 -0600

    Revert "Update vite.config.js"
    This reverts commit 94d469e75d20a93f9622572887c1626eee8ee57f.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/cardgame-website-NEW/',
-  plugins: [
-    vue(),
-    vueDevTools()
+  plugins: [vue(),
+
+    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
 })
